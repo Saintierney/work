adaaf2a2971ed9858c8b039052b9ef912b34c39f
Lloyd Saintierney <lloydtierney@hotmail.co.uk>
2021-10-02 16:53:26 +0100

    initial commit

---
+++ cities.py
@@ -0,0 +1,6 @@
+cities = dict([('England', 'Groot Brittanie'),
+              ('France', 'Frankrijk'), ('Scotland', 'Schotland')])
+
+for k, v in cities.items():
+    print("English: " + k + "\n" + "Dutch: " + v)
+    print("\n")
